1f274f1c2351c1536225bce36ca1412218ffdc0b
gustavofabro <gustavofabro.f@gmail.com>
2020-06-18 20:46:35 -0300

    test advice

--- test.ts
+++ test.ts
@@ -1,14 +1,15 @@
 import { assert } from "./test_deps.ts";
 import { killPort } from "./mod.ts";
 
+/** Provisional Go Horse unit test */
 Deno.test("it should kill the port process", async function (): Promise<void> {
-  const listener = Deno.listen({ port: 8080 });
-  await killPort(8080);
+  const listener = Deno.listen({ port: 9999 });
+  await killPort(9999);
 
   let isAddrFree = true;
 
   try {
-    Deno.listen({ port: 8080 });
+    Deno.listen({ port: 9999 });
   } catch (err) {
     if (err instanceof Deno.errors.AddrInUse) {
       isAddrFree = false;
